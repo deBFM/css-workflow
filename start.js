6d6619ffd3e45b856190724f675f2e904570d599
Dennis Kronbügel <dennis.kronbuegel@intern.hamburger-software.de>
2018-06-01 14:36:57 +0200

    Configurations shifted to nconf

--- start.js
+++ start.js
@@ -1,14 +1,59 @@
 #!/usr/bin/env node
 let shell = require('shelljs');
+let nconf = require('nconf');
 
-let cssFolder = process.env.npm_package_config_cssFolder;
-let rootFolder = process.env.npm_package_config_rootFolder;
-let upstreamHttpServer = process.env.npm_package_config_upstreamHttpServer;
-let proxyWatchFiles  = process.env.npm_package_config_proxyWatchFiles;
+//Configure nconf
+nconf.argv().file(__dirname + '/config.json');
+let workfolder = shell.pwd().stdout;
 
-shell.cd(cssFolder);
-shell.exec('sass --watch hs.scss:hs_static.css', {async: true});
+if (arefoldersAvailable()) {
+    process();
+} else {
+    console.error('At least one of the configured folders didn´t exist')
+    process.exitCode = 1;
+}
 
-shell.cd(__dirname);
-shell.cd(rootFolder);
-shell.exec(`browser-sync start --proxy "${upstreamHttpServer}" --files "${proxyWatchFiles}" --serveStatic "."`, {async: true});
+function process() {
+    if (nconf.get('build')) {
+        startSASSBuild();
+    } else {
+        startSASS();
+        startBrowserSync();
+    }
+}
+
+function arefoldersAvailable() {
+    //TODO: check all folders
+    return true; //debug
+}
+
+function cdRoot() {
+    console.log("current location before root() is: " + shell.pwd().stdout);
+    shell.cd(workfolder);
+    shell.cd(nconf.get('rootFolder'));
+    console.log("current location is: " + shell.pwd().stdout);
+}
+
+function startSASS() {
+    cdRoot();
+    shell.cd(nconf.get('cssFolder'));
+    let sassCmd = `sass --watch ${nconf.get('cssPrecompilerInputFile')}:${nconf.get('cssPrecompilerOutputFileDevelopment')}`;
+    console.log("sassCmd: " + sassCmd)
+    //shell.exec(sassCmd, {async: true});
+}
+
+function startSASSBuild() {
+    cdRoot();
+    shell.cd(nconf.get('cssFolder'));
+    let sassBuildCmd = `sass --no-cache --style compressed ${nconf.get('cssPrecompilerInputFile')} ${nconf.get('cssPrecompilerOutputFileProduction')}`;
+    console.log("sassBuildCmd: " + sassBuildCmd)
+    shell.exec(sassBuildCmd, {async: true});
+}
+
+function startBrowserSync() {
+    cdRoot();
+    let browsersyncCmd = `browser-sync start --proxy "${nconf.get('upstreamHttpServer')}" --files "${nconf.get('browsersyncWatchFiles')}" --serveStatic "."`;
+    console.log("browsersyncCmd: " + browsersyncCmd);
+    //shell.exec(browsersyncCmd, {async: true});
+}
+
